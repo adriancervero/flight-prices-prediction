8bce96cf64897f6445641a23327f613794d68de9
Adrián Cerveró <aceor93@gmail.com>
2021-04-14 18:59:31 +0200

    param for RandomizedSearchCV added

--- src/model_selector.py
+++ src/model_selector.py
@@ -1,6 +1,7 @@
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 models = {
     "decision_tree_mse": DecisionTreeRegressor(
@@ -12,4 +13,13 @@ models = {
 
     "rf": RandomForestRegressor(n_jobs=-1),
     'lr': LinearRegression(),
+}
+
+
+param_grid = {
+    "rf":{
+        "model__n_estimators": np.arange(50, 100, 10),
+        "model__max_depth": np.arange(1, 31),
+        "model__criterion": ["mse", "mae"]
+    }
 }
