9debbb81717162662892b6e09091879699efde3a
Adrián Cerveró <aceor93@gmail.com>
2021-05-06 16:06:19 +0200

    update variables for all stages

--- src/config.py
+++ src/config.py
@@ -5,24 +5,40 @@
       Configuration variables
 """
 
+RANDOM_STATE = 42
 
 # Data paths
-RAW_DATA = '../data/raw/flights_raw.csv'
+RAW_DATA_PATH = '../data/raw/flights_raw.csv'
 INTERIM_DATA_PATH = '../data/interim/flights_interim.csv'
-TRAIN_PROCESSED = "../data/processed/train.csv"
-TEST_PROCESSED = "../data/processed/test.csv"
-TRAIN_FOLDS = "../data/processed/train_folds.csv"
+TRAIN_PATH = "../data/processed/train.csv"
+TEST_PATH = "../data/processed/test.csv"
+VALID_PATH = "../data/processed/valid.csv"
+FIGURES_PATH = "../figures/"
 
+### Preprocessing variables ####
 # Day Range to use in test set when splitting
-TEST_DAYS = 14
+SPLIT_TEST_DAYS = 30
+# feature to use to combine fare
+COMBINE_PRICE_FEATURE = 'customPrice'
+# last days trend to compute customPrice and weight of this trend
+LAST_DAYS = 20
+W = 0.7
+# percentage of the minimum price drop to take into account to consider it worthwhile to wait
+MIN_DROP_PER = 0.05 
+# feature used for agg. flights
+AGG_COLS = ['orig-dest', 'airline', 'session', 'days_until_dep']
 
-# Number of folds for cross validation
-N_FOLDS = 5
 
+### Model Training variables ###
 # Feature selection
-NUMERICAL = ['days_until_dep', 'fly_duration', 'day_of_month', 'log_price', 'hops', 'competition']
-CATEGORICAL = ['flyFrom', 'flyTo', 'day_of_week', 'session']
-TARGET = ['waiting_days']
+# categorical
+CATEGORICAL = ['orig-dest', 'airline', 'session']
+
+# numerical
+NUMERICAL = ['days_until_dep', 'competition', 'prob', 'fly_duration']
+
+# target
+TARGET = 'wait'
 
 # MODEL OUTPUT
 MODEL_OUTPUT = '../models/'
